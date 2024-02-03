05d2c20d924e72c310ebe786b9d5adfefbed9fe8
Pranavi Kondapalli <pranavi.kondapalli@gmail.com>
2024-02-03 01:02:11 -0500

    adding basic functionality to route

--- app.py
+++ app.py
@@ -5,13 +5,24 @@ app = Flask(__name__)
 # states = []
 
 @app.route('/')
-def route_page():  # put route applications's code here
-    states = ["Stever", "Gates-Hillman", "Posner", "Scott", "UC"]
-    return render_template('route.html', states=states)
+def index_page():  # put route applications's code here
+    dorms = ["Stever", "Mudge", "Morewood Garden", "Morewood E-tower", "Mudge", "West Wing", "Resnik", "Donner", "The Hill"]
+    buildings = ["Gates-Hillman", "Newell-Simon", "Wean", "Scott", "Hamerschlag", "Scaife", "ANSYS", "Porter", "Baker",
+                "Hunt Library", "College of Fine Arts", "Posner", "Hall of the Arts", "Margaret Morrison", 
+                "Cohon University Center", "Purnell Center for the Arts", "Tepper", "Mellon Institute", "Doherty"]
+    return render_template('index.html', buildings=buildings, dorms = dorms)
 
-@app.route('/route')
-def index_page():  # put home applications's code here
-    return render_template('index.html')
+@app.route('/route', methods=['POST'])
+def route_page():  # put home applications's code here
+    states = []
+    states.append(request.form["dorms"])
+    states.append(request.form["class1"])
+    states.append(request.form["class2"])
+    states.append(request.form["class3"])
+    states.append(request.form["class4"])
+    print(states)
+    
+    return render_template('route.html')
 
 @app.route('/enterClasses')
 def enterClasses_page():  # put home applications's code here
