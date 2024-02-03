5906ec7ad402e22a276e8f2ad995cb7a7ce117d5
dvngishrma <devangis@andrew.cmu.edu>
2024-02-03 02:32:15 -0500

    integrated app.py searchfunction.py

--- app.py
+++ app.py
@@ -1,8 +1,71 @@
 from flask import Flask, render_template, request, flash, redirect, url_for, session
 import sqlite3
+from searchFunction import *
 
 app = Flask(__name__)
-# states = []
+states = []
+
+mealIndex=states.index("MEAL")
+states[mealIndex]=closestMeal(find(states[mealIndex-1][0], states[mealIndex-1][1]))
+
+
+path1=doSearch(states[0], states[1])
+path2=doSearch(states[1], states[2])
+path3=doSearch(states[2], states[3])
+path4=doSearch(states[3], states[4])
+path5=doSearch(states[4], states[5])
+path6=doSearch(states[5], states[0])
+
+
+
+
+pixelGrid1= iio.imread("./Search Code/cmu_map_small.png", pilmode="RGB")
+for locn in path1:
+    row=locn[0]
+    col=locn[1]
+    pixelGrid1[row][col]=[63,255,63]
+img1 = Image.fromarray(pixelGrid1.astype(np.uint8) * 255, mode='RGB')
+img1 = img1.save("route1.png")
+
+pixelGrid2= iio.imread("./Search Code/cmu_map_small.png", pilmode="RGB")
+for locn in path1:
+    row=locn[0]
+    col=locn[1]
+    pixelGrid2[row][col]=[63,255,63]
+img2 = Image.fromarray(pixelGrid2.astype(np.uint8) * 255, mode='RGB')
+img2 = img2.save("route2.png")
+
+pixelGrid3= iio.imread("./Search Code/cmu_map_small.png", pilmode="RGB")
+for locn in path1:
+    row=locn[0]
+    col=locn[1]
+    pixelGrid3[row][col]=[63,255,63]
+img3 = Image.fromarray(pixelGrid3.astype(np.uint8) * 255, mode='RGB')
+img3 = img3.save("route3.png")
+
+pixelGrid4= iio.imread("./Search Code/cmu_map_small.png", pilmode="RGB")
+for locn in path1:
+    row=locn[0]
+    col=locn[1]
+    pixelGrid4[row][col]=[63,255,63]
+img4 = Image.fromarray(pixelGrid3.astype(np.uint8) * 255, mode='RGB')
+img4 = img4.save("route4.png")
+
+pixelGrid5= iio.imread("./Search Code/cmu_map_small.png", pilmode="RGB")
+for locn in path1:
+    row=locn[0]
+    col=locn[1]
+    pixelGrid5[row][col]=[63,255,63]
+img5 = Image.fromarray(pixelGrid3.astype(np.uint8) * 255, mode='RGB')
+img5 = img5.save("route5.png")
+
+pixelGrid6= iio.imread("./Search Code/cmu_map_small.png", pilmode="RGB")
+for locn in path1:
+    row=locn[0]
+    col=locn[1]
+    pixelGrid6[row][col]=[63,255,63]
+img6 = Image.fromarray(pixelGrid6.astype(np.uint8) * 255, mode='RGB')
+img6 = img6.save("route6.png")
 
 @app.route('/')
 def index_page():  # put home applications's code here
@@ -15,7 +78,7 @@ def index_page():  # put home applications's code here
 
 @app.route('/route', methods=['POST'])
 def route_page():  # put route applications's code here
-    states = []
+    # states = []
     dormsShort = {"Stever": "STE",
             "Morewood E-tower": "MOE",
             "Morewood Garden": "MOR",
@@ -57,7 +120,7 @@ def route_page():  # put route applications's code here
     states.append(buildingsShort.get(request.form["class4"]))
     states.insert(whenMeal, "MEAL")
     print(states)
-    
+
     return render_template('route.html')
 
 @app.route('/enterClasses')
@@ -68,3 +131,6 @@ def enterClasses_page():  # put home applications's code here
     # states.append(request.form['state2'])
     # states.append(request.form['state3'])
     # states.append(request.form['state4'])
+
+
+    
